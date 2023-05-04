60d273fccc5b02ba79cf957c5ac2ef5c02bb9c8c
Ralphilou Tatoy <ralphilou24@gmail.com>
2023-05-04 16:31:07 -0700

    Changes in about page

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import "./AboutContentStyles.css";
 
 import React from "react";
-import react1 from "../assets/react1.jpg"
+import react1 from "../assets/Me.jpg"
 import react2 from "../assets/react2.jpg"
 const AboutContent = () => {
   return <div className="AboutPage">
@@ -11,7 +11,7 @@ const AboutContent = () => {
         Who Am I?
       </h1>
       <p>
-        I'm a Software Engineer. I have knowledge of front-end and back-end developing experience. I create software through agile software development.
+        My name is Ralphilou Tatoy. I'm a Software Engineer. I have knowledge of front-end and back-end developing experience. I create software through agile software development.
       </p>
       <Link to="/Contact">
         <button className="button1">Contact</button>
