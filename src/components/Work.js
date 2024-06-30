60eabd3c5bd8b62500ed6e8b11959266e7bf3c5d
Ralphilou Tatoy <ralphilou24@gmail.com>
2024-06-29 22:14:40 -0700

    Added sorting to projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WorkCardStyles.css";
 import WorkCard from "./WorkCard"
 import WorkCardData from "./WorkCardData";
 
 const Work = () => {
+  const [sortState, setSortState] = useState("none");
+
+  const sorting = {
+    none: {method: (a,b) => null},
+    ascending: {method: (a,b) => (a.title < b.title ? -1 : 1)},
+    descending: {method: (a,b) => (a.title > b.title ? -1 : 1)}
+  };
+
   return <div className="WorkGrid">
     <h1 className="WorkHeading">
-        Projects
+        Projects 
+        <div>
+    <select defaultValue={'DEFAULT'} onChange={(e)=> setSortState(e.target.value)} className="button2">
+          <option value="DEFAULT" disabled>Sort</option>
+          <option value="ascending">Ascending</option>
+          <option value="descending">Descending</option>
+        </select>
+    </div>
     </h1>
+    
     <div className="WorkContainer">
-        {WorkCardData.map((value,index)=>{
+        
+        {WorkCardData.sort(sorting[sortState].method).map((value,index)=>{
           return(
             <WorkCard key={index} imgsrc={value.imgsrc} title={value.title} content={value.content} source = {value.source} indepthsummary = {value.indepthsummary} summaryLink = {value.summaryLink}/>
           )
